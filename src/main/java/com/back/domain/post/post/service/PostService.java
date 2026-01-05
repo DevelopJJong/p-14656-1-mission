0de5082d7a70c9d49e2e2221e4a0e787e4a60032
이현종 <101362306+DevelopJJong@users.noreply.github.com>
2026-01-05 16:09:44 +0900

    0010 : Post 엔티티 및 Repository 생성

--- src/main/java/com/back/domain/post/post/service/PostService.java
+++ src/main/java/com/back/domain/post/post/service/PostService.java
@@ -1,10 +1,16 @@
 package com.back.domain.post.post.service;
 
+import com.back.domain.post.post.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@RequiredArgsConstructor
 public class PostService {
+    private PostRepository postRepository;
+
     public long count() {
-        return 0;
+        return postRepository.count();
     }
 }
