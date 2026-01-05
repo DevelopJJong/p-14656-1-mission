f1243f797445a18d6a1127c7ee9a4dc5e86e999f
이현종 <101362306+DevelopJJong@users.noreply.github.com>
2026-01-05 16:18:20 +0900

    0011 : Post 생성구현(create)

--- src/main/java/com/back/domain/post/post/service/PostService.java
+++ src/main/java/com/back/domain/post/post/service/PostService.java
@@ -1,5 +1,6 @@
 package com.back.domain.post.post.service;
 
+import com.back.domain.post.post.document.Post;
 import com.back.domain.post.post.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -8,9 +9,14 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @RequiredArgsConstructor
 public class PostService {
-    private PostRepository postRepository;
+    private final PostRepository postRepository;
 
     public long count() {
         return postRepository.count();
+    }
+
+    public Post create(String title, String content, String author) {
+        Post post = new Post(title, content, author);
+        return postRepository.save(post);
     }
 }
